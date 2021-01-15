ece9a3f9a7d21a93a8c075115fbb6d30d85af316
kolton thompson <koltonthompson@koltons-MacBook-Pro.local>
2021-01-15 14:39:00 -0700

    first commit

---
+++ tip_calculator.js
@@ -0,0 +1,22 @@
+// var today = new Date();
+// var hourNow = today.getHours();
+// var greeting;
+
+// if (hourNow > 18) {
+//   greeting = "Good evening";
+// } else if (hourNow > 12) {
+//   greeting = "Good afternoon";
+// } else if (hourNow > 0) {
+//   greeting = "Good morning";
+// } else {
+//   greeting = "Welcome";
+// }
+// document.write(greeting + "! Are all the hangrys gone?");
+
+let billAmount = 50
+
+let tip = billAmount 
+
+
+console.log(billAmount);
+console.log(tip);
